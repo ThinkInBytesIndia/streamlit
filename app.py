c6152474d9d8a0771fe459c929382b240de48a81
Darshil Modi <45987056+darshil3011@users.noreply.github.com>
2022-10-18 15:45:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,13 +110,4 @@ if __name__ == "__main__":
     main()
     st.markdown("<h2 style='text-align: center; color: black;'>Image Classification - Applications</h2>", unsafe_allow_html=True)
     image = Image.open('screen1.png')
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.write(' ')
-
-    with col2:
-        st.image(image)
-
-    with col3:
-        st.write(' ')
+    st.image(image)
